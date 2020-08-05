42ab44df711494b6756c20ea969473e9703f0273
Brenda <apples@BrendaJ.local>
2020-08-06 00:17:17 +0300

    chore: setup migration

---
+++ knexfile.js
@@ -0,0 +1,24 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/user.db3'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+  },
+
+};
