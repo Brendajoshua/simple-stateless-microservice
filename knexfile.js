a617008b88f18f64ef52565db622e3b7e18cdfc6
Brenda <apples@BrendaJ.local>
2020-08-06 00:28:21 +0300

    feat: add auth-router

--- knexfile.js
+++ knexfile.js
@@ -4,6 +4,7 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
       filename: './database/user.db3'
     },
@@ -12,7 +13,6 @@ module.exports = {
         conn.run('PRAGMA foreign_keys = ON', done);
       },
     },
-    useNullAsDefault: true,
     migrations: {
       directory: './database/migrations',
     },
